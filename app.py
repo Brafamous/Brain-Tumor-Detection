5b959e615caecbb39bd424936461870ea126370a
FAMOUS <famoustay55@gmail.com>
2023-08-08 18:13:33 -0400

    New files being added for this project

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import cv2
+from keras.models import load_model
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+
+
+
+#Loading the Model
+model = load_model('Brain_Tumor_Detection_1.0')
+
+
+def get_className(classNo):
+	if classNo==0:
+		return "There is NO tumor in the brain"
+	elif classNo==1:
+		return "There is tumor in the brain"
+
+
+def getResult(img):
+    image=cv2.imread(img)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize((64, 64))
+    image=np.array(image)
+    input_img = np.expand_dims(image, axis=0)
+    result=model.predict(input_img)
+    return result
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        value=getResult(file_path)
+        result=get_className(value) 
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
